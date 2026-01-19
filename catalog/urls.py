8d3ee8aa114621889f391d3f83d961b975f4d329
Akmal Khamdullaev <akm992@mai.ru>
2026-01-19 15:16:34 +0300

    реализация  26.1: CRUD с валидацией запрещенных слов и цены, стилизация форм

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,18 +4,31 @@ from django.conf.urls.static import static
 from .views import (
     HomeView,
     ContactsView,
+    ProductListView,
     ProductDetailView,
     ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
     TestView
 )
 
 app_name = 'catalog'
 
 urlpatterns = [
+    # Главная страница
     path('', HomeView.as_view(), name='home'),
+
+    # Контакты
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('product/<int:product_id>/', ProductDetailView.as_view(), name='product_detail'),
-    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+
+    # CRUD для продуктов (ВСЕ 5 операций)
+    path('products/', ProductListView.as_view(), name='product_list'),  # СПИСОК
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),  # СОЗДАНИЕ
+    path('product/<int:product_id>/', ProductDetailView.as_view(), name='product_detail'),  # ПРОСМОТР
+    path('product/<int:product_id>/update/', ProductUpdateView.as_view(), name='product_update'),  # РЕДАКТИРОВАНИЕ
+    path('product/<int:product_id>/delete/', ProductDeleteView.as_view(), name='product_delete'),  # УДАЛЕНИЕ
+
+    # Тестовый URL
     path('test/', TestView.as_view(), name='test'),
 ]
 
